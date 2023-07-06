d89a7dfcfc68680142b8a15a378cb8ef4f9d01d6
Eugene <orion25rus@gmail.com>
2023-07-07 00:12:04 +0300

    Fix: Module threw because of client_ready() missing 1 required positional argument: 'client'

--- Always_Online.py
+++ Always_Online.py
@@ -11,21 +11,26 @@ __version__ = (0, 1, 0)
 # scope: hikka_only
 
 import datetime
+import logging
 
 from telethon import types, functions
 from telethon.tl.types import UpdateUserStatus
 
 from .. import loader, utils
 
+logger = logging.getLogger(__name__)
+
 
 @loader.tds
 class AlwaysOnlineMod(loader.Module):
+    """Вечный онлайн"""
 
     strings = {"name": "AlwaysOnline"}
+    strings_ru = {"_cls_doc": "Вечный онлайн"}
     last_online = 0
     per = 2 * 60 - 10
 
-    async def client_ready(self, client) -> None:
+    async def client_ready(self, client, db) -> None:
         self.client = client
         self.me = await client.get_me()
         self.uid = self.me.id
@@ -56,13 +61,16 @@ class AlwaysOnlineMod(loader.Module):
     async def scheduler(self) -> None:
         if self.last_online != 0 and self.last_online + self.per <= datetime.datetime.now().timestamp():
             await self.go_online()
+            # logger.info('Вышли в онлайн через go_online()')
 
     @loader.raw_handler(UpdateUserStatus)
     async def update_handler(self, update: UpdateUserStatus):
 
         if update.user_id == self.uid and type(update.status) == types.UserStatusOffline:
             await self.go_online()
+            # logger.info('Пользователь вышел в оффлайн')
 
         elif update.user_id == self.uid and type(update.status) == types.UserStatusOnline:
             self.last_online = 0
+            # logger.info('Пользователь вышел в онлайн')
 
