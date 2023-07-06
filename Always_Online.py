88a1c7aea2bc36afd9e57856caa53c0f1d9ecff1
Eugene <orion25rus@gmail.com>
2023-07-07 00:09:04 +0300

    Refactor

--- Always_Online.py
+++ Always_Online.py
@@ -1,42 +1,27 @@
-__version__ = (1, 0, 2)
+"""
+                    © Copyright 2023
+                    https://izxv.fun
+                Licensed under the MPL 2.0
+          https://www.mozilla.org/en-US/MPL/2.0/
+"""
 
-#
-#     d88P     d88P          888b    888          888
-#      d88P   d88P           8888b   888          888
-#       d88P d88P            88888b  888          888
-#        d88888P    888888   888Y88b 888  .d88b.  888888
-#        d88888P    888888   888 Y88b888 d8P  Y8b 888
-#       d88P d88P            888  Y88888 88888888 888
-#      d88P   d88P           888   Y8888 Y8b.     Y88b.
-#     d88P     d88P          88.8    Y888  "Y8888   "Y888
-#
-#                      © Copyright 2022
-#                    https:// x-net.pp.ua
-#
-#                 Licensed under the GNU GPLv3
-#          https:// www.gnu.org/licenses/agpl-3.0.html
-#
+__version__ = (0, 1, 0)
 
-# meta developer: @zxcghost666
+# meta developer: @eugconrad
 # scope: hikka_only
-# scope: hikka_min 1.2.10
+
 import datetime
-import logging
 
 from telethon import types, functions
 from telethon.tl.types import UpdateUserStatus
 
 from .. import loader, utils
 
-logger = logging.getLogger(__name__)
-
 
 @loader.tds
 class AlwaysOnlineMod(loader.Module):
-    """Вечный онлайн"""
 
     strings = {"name": "AlwaysOnline"}
-    strings_ru = {"_cls_doc": "Вечный онлайн"}
     last_online = 0
     per = 2 * 60 - 10
 
@@ -71,16 +56,13 @@ class AlwaysOnlineMod(loader.Module):
     async def scheduler(self) -> None:
         if self.last_online != 0 and self.last_online + self.per <= datetime.datetime.now().timestamp():
             await self.go_online()
-            # logger.info('Вышли в онлайн через go_online()')
 
     @loader.raw_handler(UpdateUserStatus)
     async def update_handler(self, update: UpdateUserStatus):
 
         if update.user_id == self.uid and type(update.status) == types.UserStatusOffline:
             await self.go_online()
-            # logger.info('Пользователь вышел в оффлайн')
 
         elif update.user_id == self.uid and type(update.status) == types.UserStatusOnline:
             self.last_online = 0
-            # logger.info('Пользователь вышел в онлайн')
 
